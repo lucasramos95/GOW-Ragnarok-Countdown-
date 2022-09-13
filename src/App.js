e3877de4aa06e53e4c55c2ba14e7e34ec622f962
Lucas Ramos <lucas_ramos95@outlook.com>
2022-09-13 01:48:01 -0300

    3rd commit - Título HTML

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <img src='./background.jpg' className='background'/>
+      <h2>GOD OF WAR RAGNAROK - LANÇAMENTO: 09/11/2022</h2>
     </div>
   );
 }
