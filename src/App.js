f18b35e37680248d162c0e8aef085d0ce3fe640c
Lucas Ramos <lucas_ramos95@outlook.com>
2022-09-14 15:14:11 -0300

    4th Commit - componente do cronômetro feito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { useCountdown } from './countdown';
 
 function App() {
   return (
     <div className="App">
       <img src='./background.jpg' className='background'/>
       <h2>GOD OF WAR RAGNAROK - LANÇAMENTO: 09/11/2022</h2>
+      <>{useCountdown}</>
     </div>
   );
 }
