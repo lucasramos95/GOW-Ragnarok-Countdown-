06dbd1b9d87b75548e0d87980b082b4f2ca5fcc9
Lucas Ramos <lucas_ramos95@outlook.com>
2022-09-14 16:04:23 -0300

    5th Commit - Hooks + componentes Timer e Display/Date/Time

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React from 'react';
+import CountdownTimer from './CountdownTimer';
 import './App.css';
-import { useCountdown } from './countdown';
 
-function App() {
+export default function App() {
+  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+
+  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+
   return (
-    <div className="App">
-      <img src='./background.jpg' className='background'/>
-      <h2>GOD OF WAR RAGNAROK - LANÇAMENTO: 09/11/2022</h2>
-      <>{useCountdown}</>
+    <div className='App'>
+      <img className='background' src='./background.jpg'/>
+      <h2>GOD OF WAR RAGNAROK - CONTAGEM DE LANÇAMENTO - 09/11/2022</h2>
+      <div className='countdownText'>
+        <CountdownTimer targetDate={dateTimeAfterThreeDays} />
+      </div>
+      
     </div>
   );
 }
-
-export default App;
--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { useEffect, useState } from 'react';
 
 const useCountdown = (targetDate) => {
