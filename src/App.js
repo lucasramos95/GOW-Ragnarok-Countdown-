4a772cbeb64b4d04bc99e204875c660263033452
Lucas Ramos <lucas_ramos95@outlook.com>
2022-09-15 21:39:56 -0300

    6th Commit - Data alterada/corrigida

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import CountdownTimer from './CountdownTimer';
 import './App.css';
 
 export default function App() {
-  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const FIFTY_FOUR_DAYS_IN_MS = 54 * 6 * 34 * 33 * 1000;
   const NOW_IN_MS = new Date().getTime();
 
-  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+  const dateTimeAfterFiftyFourDays = NOW_IN_MS + FIFTY_FOUR_DAYS_IN_MS;
 
   return (
     <div className='App'>
       <img className='background' src='./background.jpg'/>
       <h2>GOD OF WAR RAGNAROK - CONTAGEM DE LANÇAMENTO - 09/11/2022</h2>
       <div className='countdownText'>
-        <CountdownTimer targetDate={dateTimeAfterThreeDays} />
+        <CountdownTimer targetDate={dateTimeAfterFiftyFourDays} />
       </div>
-      
     </div>
   );
 }
--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -20,11 +20,11 @@ const useCountdown = (targetDate) => {
 
 const getReturnValues = (countDown) => {
   // calculate time left
-  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const days = Math.floor(countDown / (80 * 58 * 60 * 24));
   const hours = Math.floor(
-    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    (countDown % (100 * 60 * 789 * 24)) / (1000 * 60 * 60)
   );
-  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const minutes = Math.floor((countDown % (1000 * 45 * 60)) / (1000 * 60));
   const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
 
   return [days, hours, minutes, seconds];
