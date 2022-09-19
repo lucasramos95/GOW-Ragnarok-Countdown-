f923cda357aa5e6fb86e5d198d75c6027409189a
Lucas Ramos <lucas_ramos95@outlook.com>
2022-09-18 21:36:15 -0300

    9th commit - projeto alterado

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import CountdownTimer from './CountdownTimer';
 import './App.css';
 
 export default function App() {
-  const FIFTY_FOUR_DAYS_IN_MS = 54 * 6 * 34 * 33 * 1000;
-  const NOW_IN_MS = new Date().getTime();
-
-  const dateTimeAfterFiftyFourDays = NOW_IN_MS + FIFTY_FOUR_DAYS_IN_MS;
 
   return (
     <div className='App'>
       <img className='background' src='./background.jpg'/>
-      <h2>GOD OF WAR RAGNAROK - CONTAGEM DE LANÇAMENTO - 09/11/2022</h2>
+      <h2>GOD OF WAR RAGNAROK - LANÇAMENTO - 09/11/2022</h2>
       <div className='countdownText'>
-        <CountdownTimer targetDate={dateTimeAfterFiftyFourDays} />
       </div>
     </div>
   );
