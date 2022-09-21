844778bbe16b7f73cb91582d7e701da5aea0b4a3
Lucas Ramos <lucas_ramos95@outlook.com>
2022-09-20 23:34:36 -0300

    11th Commit - novas imagens adicionadas

--- src/App.js
+++ src/App.js
@@ -2,25 +2,7 @@ import React from 'react';
 import './App.css';
 import Carousel from 'better-react-carousel';
 
-const Gallery = () => {
-  return (
-    <Carousel cols={2} rows={1} gap={10} loop>
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=1" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=2" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=3" />
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* anything you want to show in the grid */}
-      </Carousel.Item>
-      {/* ... */}
-    </Carousel>
-  )
-}
+
 
 export default function App() {
   return (
